1b949c96b154ce06984cb02a22950b4b4be7ae27
xvvcs <maciek.m2004@wp.pl>
2024-03-03 18:29:19 +0100

    DISPLAYING IN TABLE FIXED!!!!
    goat

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,9 @@ module com.example.assigmentsdj {
     opens com.example.assigmentsdj to javafx.fxml;
     opens com.example.assigmentsdj.View to javafx.fxml;
     exports com.example.assigmentsdj;
+
+
+    opens com.example.assigmentsdj.Model to javafx.base;
+    opens com.example.assigmentsdj.ViewModel to javafx.base;
+
 }
